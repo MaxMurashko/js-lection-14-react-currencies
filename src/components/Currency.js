c46455e2d658cf1478cf72394daf7ad9c889a303
MaxMurashko <murashko.maksym@gmail.com>
2022-12-12 07:00:44 +0200

    add toFixed to currency

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -3,7 +3,7 @@ import React from "react";
 function Currency({currency}) {
     return <tr>
         <td className="text-center">{currency.name}</td>
-        <td className="text-center text-danger">{currency.rate}</td>
+        <td className="text-center text-danger">{currency.rate.toFixed(4)}</td>
 </tr>
 }
 
